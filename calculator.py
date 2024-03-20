0124421e5247d49d70932435bab17b8ef4d3e5cd
Caio Seniuk <116319578+CaioSeniuk@users.noreply.github.com>
2024-03-20 10:43:08 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,38 @@
+#print(f"\nProgram that calculate basic mathematic operations!")
+#num1 = float(input("\nEnter the 1º number: "))
+#num2 = float(input("\nEnter the 2º number: "))
+#op = int(input("\nWich operation do you want to do? \n1 - addition \n2 - subtraction \n3 - multiplication \n4 - division \n5 - quit\n: "))
+#if op== 1:
+#   print(f"\nThe calculation is {num1} + 2{num2}= {num1+num2:.2f}\n")
+#elif op== 2:
+#    print(f"\nThe calculation is {num1} - {num2}= {num1 - num2:.2f}\n")
+#elif op== 3:
+#    print(f"\nThe calculation is {num1} x {num2}= {num1 * num2:.2f}\n")
+#elif op== 4:
+#    print(f"\nThe calculation is {num1} / {num2}= {num1 / num2:.2f}\n")
+#else:
+#    quit()
+#New project with (while else)
 print(f"\nProgram that calculate basic mathematic operations!")
-num1 = float(input("\nEnter the 1º number: "))
-num2 = float(input("\nEnter the 2º number: "))
-op = int(input("\nWich operation do you want to do? \n1 - addition \n2 - subtraction \n3 - multiplication \n4 - division \n5 - quit\n: "))
-if op== 1:
-    print(f"\nThe calculation is {num1} + 2{num2}= {num1+num2:.2f}\n")
-elif op== 2:
-    print(f"\nThe calculation is {num1} - {num2}= {num1 - num2:.2f}\n")
-elif op== 3:
-    print(f"\nThe calculation is {num1} x {num2}= {num1 * num2:.2f}\n")
-elif op== 4:
-    print(f"\nThe calculation is {num1} / {num2}= {num1 / num2:.2f}\n")
+op = 0
+while op < 5:
+    op = int(input("\nWich operation do you want to do? \n1 - addition \n2 - subtraction \n3 - multiplication \n4 - division \n5 - quit\n\n:  "))
+    if op== 1:
+        num1 = float(input("\nEnter the 1º number: "))
+        num2 = float(input("\nEnter the 2º number: "))
+        print(f"\nThe calculation is {num1} + 2{num2} = {num1+num2:.2f}\n" + ("-"*60))
+    elif op== 2:
+        num1 = float(input("\nEnter the 1º number: "))
+        num2 = float(input("\nEnter the 2º number: "))
+        print(f"\nThe calculation is {num1} - {num2} = {num1 - num2:.2f}\n" + ("-"*60))
+    elif op== 3:
+        num1 = float(input("\nEnter the 1º number: "))
+        num2 = float(input("\nEnter the 2º number: "))
+        print(f"\nThe calculation is {num1} x {num2} = {num1 * num2:.2f}\n" + ("-"*60))
+    elif op== 4:
+        num1 = float(input("\nEnter the 1º number: "))
+        num2 = float(input("\nEnter the 2º number: "))
+        print(f"\nThe calculation is {num1} / {num2} = {num1 / num2:.2f}\n" + ("-"*60))
 else:
-    quit()
+    print("\nFim do programa !\n"+ ("-"*60))
+    quit
